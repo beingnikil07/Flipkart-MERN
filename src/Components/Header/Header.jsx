81f89b7a95765fc9366c17fbfb2e3a2a47883be1
Nikil kumar <kumarnikil220@gmail.com>
2023-01-16 15:37:31 +0530

    Flipkart logo created

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
-const Header=()=>{
+import React from 'react';
+import { AppBar, Toolbar, styled, Box, Typography } from '@mui/material';
+const StyledHeader = styled(AppBar)`
+        background:#2874f0;
+        height:55px;
+`;
+const Component = styled(Box)`
+    margin-left:12%;
+    line-height:0;
+`;
+const SubHeading = styled(Typography)`
+    font-size:10px;
+    font-style:italic;
+`;
+const PlusLogo = styled('img')({
+    width: 10,
+    height: 10,
+    marginLeft: 4
+})
+const logoUrl = "https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png";
+const subUrl = "https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/plus_aef861.png";
+const Header = () => {
     return (
-        <h1>I'm from Header</h1>
+        <StyledHeader>
+            <Toolbar>
+                <Component>
+                    <img src={logoUrl} alt="logo" style={{ width: 75 }} />
+                    <Box style={{ display: 'flex' }}>
+                        <SubHeading>Explore&nbsp;
+                            <Box component="span" style={{ color: '#ffe500' }}>Plus</Box>
+                        </SubHeading>
+                        <PlusLogo src={subUrl} alt="sub-logo" />
+                    </Box>
+                </Component>
+            </Toolbar>
+        </StyledHeader>
     )
 }
 export default Header;
