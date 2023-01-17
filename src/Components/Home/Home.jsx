38b8aa940713f867b0d0832834c384032ad2c4ec
Nikil kumar <kumarnikil220@gmail.com>
2023-01-17 12:52:46 +0530

    background color added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,18 @@
 //Components..
 import Banner from "./Banner";
 import NavBar from "./NavBar";
+import { Box, styled } from '@mui/material';
+const Wrapper = styled(Box)`
+    padding:10px 10px;
+    background:#f2f2f2;
+`;
 const Home = () => {
     return (
         <>
             <NavBar />
-            <Banner />
+            <Wrapper>
+                <Banner />
+            </Wrapper>
         </>
     )
 };
