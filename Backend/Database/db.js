0e669d89a6ddca9509c31e8b55e3d7e44fdca33f
Nikil kumar <kumarnikil220@gmail.com>
2023-01-17 13:52:25 +0530

    username and password have been hidden

--- Backend/Database/db.js
+++ Backend/Database/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-export const Connection = async () => {
-    const URL = "mongodb+srv://nikhil:nik12345@flipkart-crud.lrb1w9f.mongodb.net/?retryWrites=true&w=majority";
+export const Connection = async (username, password) => {
+    const URL = `mongodb+srv://${username}:${password}@flipkart-crud.lrb1w9f.mongodb.net/?retryWrites=true&w=majority`;
     try {
         mongoose.set('strictQuery', false);
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import {Connection} from './Database/db.js';  
-
-
-const app = express();  //initializing 
+import { Connection } from './Database/db.js';
+import dotenv from 'dotenv';
 const PORT = 8000;
+const app = express();  //initializing 
 
-Connection();    //calling database
+dotenv.config();  //it is must to configure dotenv. 
+const username=process.env.DB_USERNAME;
+const password=process.env.DB_PASSWORD;
+Connection(username,password);    //calling database
 app.listen(PORT, () => {
     console.log(`server running successfully on port ${PORT}`);
 })
