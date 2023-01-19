df5fb9bbcf3708571fa2fd44218ab7ff06d9129f
Nikil kumar <kumarnikil220@gmail.com>
2023-01-19 14:16:14 +0530

    post api body parsing problem resolved

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import defaultData from './default.js'; //importing default data 
 import Routes from './Routes/route.js';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const PORT = 5000;
 const app = express();  //initializing 
@@ -11,6 +12,8 @@ const app = express();  //initializing
 dotenv.config();  //it is must to configure dotenv.
 
 app.use(cors());
+app.use(bodyParser.json({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
 //using routes 
 // '/' it is a default route you can also change it for requirement
 //Router likne ka mean ye saare router ko use karega  
