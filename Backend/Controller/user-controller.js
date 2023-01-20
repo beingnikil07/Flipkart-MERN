4935de1cefb3f3c96b6ad1af643b0ef2db1b0746
Nikil kumar <kumarnikil220@gmail.com>
2023-01-20 10:05:26 +0530

    Handled user existence in database

--- Backend/Controller/user-controller.js
+++ Backend/Controller/user-controller.js
@@ -2,18 +2,17 @@ import User from '../schema/user-schema.js';
 
 export const userSignUp = async (req, res) => {
     try {
-        //saving request data to variable to ye data mujhpr front end se aa rha hai to mere ko isko                         
-        // validate karna padega to hum phir se user ke liye ek schema banayenge
+        const exist=await User.findOne({username:req.body.username});
+        if(exist){
+            return res.status(401).json({message:"username already exist :("});
+        }
         const user = req.body;
-        const newUser = new User(user);   //passing user data to the User schema 
-        await newUser.save();    //To store the data into mongo database
-        // Avv agar data save ho jaata hai to hum uss api ko ek response send krr denge with
-        // status code ke sath
+        const newUser = new User(user); 
+        await newUser.save();    
         res.status(200).json({ message: user });
 
     } catch (error) {
-        //Avv agar save karte hue koi error aayegi to mai usko bhi response ke through send krr dunga
-        //api ko
+        
         res.status(500).json({ message: error.message });
     }
 };
