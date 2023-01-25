b3a1808c6f3276f7439199ff7427d0712a5ffda9
Nikil kumar <kumarnikil220@gmail.com>
2023-01-25 10:27:12 +0530

    products data stored in redux successfully

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getProducts } from '../../redux/actions/productActions'; //isme hamari api rakhi hui hai 
+import { useSelector } from "react-redux";
 //Components..
 import Banner from "./Banner";
 import NavBar from "./NavBar";
@@ -7,6 +11,20 @@ const Wrapper = styled(Box)`
     background:#f2f2f2;
 `;
 const Home = () => {
+    //object destructuring from getProducts redux state 
+   const {products}= useSelector(state => state.getProducts); //ye getProducts reducer wala nii ye redux kii state hai 
+   console.log(products);   //this will show you all products 
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getProducts());
+        /* dispatch maim maine call kiye ye jayege avv iss pass wale kii defination prr 
+           ,[]mai dispatch isliye diya jisse dispatch ke change hote he ye hamra change ho jaaye 
+           mean ye dene ye useeffect hamara componentDidupdtae kii tarah behave karega
+        */
+    }, [dispatch]);
+
+
     return (
         <>
             <NavBar />
